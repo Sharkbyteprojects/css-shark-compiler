603147be535267ab75a3400bfb3f4052e9986e72
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2022-01-30 20:22:46 +0100

    Update sjs.cpp

--- sjs.cpp
+++ sjs.cpp
@@ -2,8 +2,8 @@
 #include "cssshark.hpp"
 #include <string>
 
-int main(unsigned int argc, char* argv[]){
-    for(unsigned int c = 1; c < argc; c++){
+int main(int argc, char* argv[]){
+    for(int c = 1; c < argc; c++){
         cssd(argv[c], (std::string(argv[c]) + std::string(".out.css")).c_str());
     }
     return 0;
